b83e08e0638a6ce11ce3fd1ba24253a6bb71decc
narawaa <nashwa.ghania@ui.ac.id>
2024-09-15 00:24:04 +0700

    feat: Tugas 3 - add uuid

--- main/models.py
+++ main/models.py
@@ -1,11 +1,12 @@
 from django.db import models
+import uuid
 
 class Product(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255)
     price = models.IntegerField()
     description = models.TextField(blank=True, null=True)
     time = models.DateField(auto_now_add=True)
-    # image = models.ImageField()
     stock = models.IntegerField()
 
     def __str__(self):
